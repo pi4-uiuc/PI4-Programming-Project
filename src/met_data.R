1d664efe93dcdca48b59caa95139fa8d0e9e81bc
David LeBauer <dlebauer@illinois.edu>
2018-05-24 21:26:48 +0000

    added code to write out met data

--- src/met_data.R
+++ src/met_data.R
@@ -8,7 +8,9 @@ library(lubridate)
 # can use parameters &since=2016-03-01&until=2018-12-31 to subset data
 
 rawmet <- list()
-for(stream_id in c(3211, 4806, 4807, 4805)){
+#3211 is AZ
+
+for(stream_id in c(4806, 4807, 4805)){
   z <- fromJSON(paste0("https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?stream_id=", stream_id))
   time <-  z %>% 
     dplyr::select(start_time, end_time) %>% 
@@ -21,38 +23,5 @@ for(stream_id in c(3211, 4806, 4807, 4805)){
   rawmet[[sitename[1]]] <- bind_cols(time, site = sitename, properties)
 }
 
-
-library(jsonlite)
-library(lubridate)
-library(dplyr)
-library(ggplot2)
-library(purrr)
-library(fitdistrplus)
-
-
-uiuc_weather_raw3 <- fromJSON(txt = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?key=Pb3AUSqnUw&stream_id=4805&since=2016-03-01&until=2018-12-31")
-uiuc_weather3 <- uiuc_weather_raw3$properties
-"
-
-system.time(
-uiuc_time3 <- uiuc_weather_raw3 %>% 
-  select(start_time, end_time) %>% 
-  mutate(start = ymd_hms(start_time), end = ymd_hms(end_time)) %>% 
-  rowwise() %>% 
-  transmute(time = mean(c(start, end)))
-)
-
-
-uiuc_actual_times <- 
-  map2_df(uiuc_time3$start_time, uiuc_time3$end_time, 
-          (function (x,y) mean(c(ymd_hms(x),ymd_hms(y))))
-
-uiuc_actual_times[[1]]
-
-# The above is a POSIX time, as desired
-
-uiuc_actual_times.df <- data.frame(unlist(uiuc_actual_times))
-
-uiuc_actual_times.df[[1]]
-
-# I would like to (and can) perform a bind_cols() operation with uiuc_actual_times.df now, but as you can see, the entries are not being recognized as POSIX values
+z <- bind_rows(rawmet)
+readr::write_csv(z, path = 'data/uiuc_met.csv')
