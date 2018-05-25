0bfe93f1959e9fba8e6bbc6b403e2ea8dbf98a51
Adriana Morales <adriana6@illinois.edu>
2018-05-25 14:39:51 +0000

    Update of Visuals and Algorithm files

---
+++ AlgorithmForCorruptData.R
@@ -0,0 +1,7 @@
+corrupt_temp <- function (air_temperature,upper_bound, lower_bound){
+  
+  if(air_temperature > low_bound && air_temperature < upp_bound) {
+    return(FALSE)
+  } else {return(TRUE)}
+}
+
--- InitialVisualwithConstraints.R
+++ InitialVisualwithConstraints.R
@@ -6,67 +6,98 @@ uiuc_weather_raw1 <- fromJSON(txt = "https://terraref.ncsa.illinois.edu/clowder/
 
 uiuc_weather1 <- uiuc_weather_raw1$properties
 
-uiuc_weather_raw2 <- fromJSON(txt = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?key=Pb3AUSqnUw&stream_id=4805&since=2016-03-01&until=2018-12-31")
+uiuc_weather_raw2 <- fromJSON(txt = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?key=Pb3AUSqnUw&stream_id=4807&since=2016-03-01&until=2018-12-31")
 uiuc_weather2 <- uiuc_weather_raw2$properties
-
-
-## This isn't the data station we're looking for; LeBauer told us there would be a third station that looks like the above, but this one must be incorrect; please fix
   
-uiuc_weather_raw3 <- fromJSON(txt = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?key=Pb3AUSqnUw&stream_id=4808&since=2016-03-01&until=2018-12-31")
+uiuc_weather_raw3 <- fromJSON(txt = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?key=Pb3AUSqnUw&stream_id=4808since=2016-03-01&until=2018-12-31")
+uiuc_weather3 <- uiuc_weathe_raw3$properties
 
+#DATA SET 1
 attach(uiuc_weather_raw1)
 attach(properties)
 
-plot(air_temperature, ylim=c(215,350))
-abline(a=233.15,b=0)
+#plot(air_temperature, ylim=c(215,350))
+#abline(a=233.15,b=0)
 
 #Air temperature Plot
-plot(air_temperature, ylim=c(215,350),main = "Air temperature through time", xlab = "Time", ylab= "Air Temperature (K)" , type= 'l', col= 'blue')
-abline(a=233.15, b= 0, col='red')
- abline(a= 343.15, b=0, col= 'green')
-
-
-plot(relative_humidity, ylim= c(0, 100), type = 'l', col='blue')
- abline(a=0, b=0, col= 'red')
- abline(a=95, b=0, col='green')
+plot(air_temperature, ylim=c(100,350),main = "Air temperature through time", xlab = "Time", ylab= "Air Temperature (K)" , type= 'l', col= 'blue')
+abline(a=193.15, b= 0, col='red')
+abline(a=333.15 , b=0, col= 'green')
 
  #Relative humidity Plot
- plot(relative_humidity, ylim= c(0, 100), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
+ plot(relative_humidity, ylim= c(0, 105), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
  abline(a=0, b=0, col= 'red')
- abline(a=95, b=0, col='green')
- abline(a=95.25, b=0, col= 'purple')
+ abline(a=100, b=0, col='green')
+ #abline(a=95.25, b=0, col= 'purple')
  
- #zoom of relative humidity
- plot(relative_humidity, ylim= c(94, 99), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
-  abline(a=0, b=0, col= 'red')
- abline(a=95, b=0, col='green')
- abline(a=95.25, b=0, col= 'red')
- 
+detach(properties)
  detach(uiuc_weather_raw1)
+ #############################################################################################################################################
+ 
+ 
+ #DATA SET 2 
  attach(uiuc_weather_raw2)
- attach(properties)
+attach(properties)
 
  #Air temperature Plot
- plot(air_temperature, ylim=c(215,350),main = "Air temperature through time", xlab = "Time", ylab= "Air Temperature (K)" , type= 'l', col= 'blue')
- abline(a=233.15, b= 0, col='red')
- abline(a= 343.15, b=0, col= 'green')
+ plot(air_temperature, ylim=c(100,350),main = "Air temperature through time", xlab = "Time", ylab= "Air Temperature (K)" , type= 'l', col= 'blue')
+ abline(a=193.15, b= 0, col='red')
+ abline(a= 333.15, b=0, col= 'green')
  
  
- plot(relative_humidity, ylim= c(0, 100), type = 'l', col='blue')
+ plot(relative_humidity, ylim= c(-105, 105), type = 'l', col='blue')
  abline(a=0, b=0, col= 'red')
- abline(a=95, b=0, col='green')
+ abline(a=100, b=0, col='green')
  
  #Relative humidity Plot
- plot(relative_humidity, ylim= c(0, 100), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
+ plot(relative_humidity, ylim= c(-105, 105), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
  abline(a=0, b=0, col= 'red')
- abline(a=95, b=0, col='green')
- abline(a=95.25, b=0, col= 'purple')
+ abline(a=100, b=0, col='green')
+ #abline(a=95.25, b=0, col= 'purple')
  
  #zoom of relative humidity
- plot(relative_humidity, ylim= c(94, 99), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
+ plot(relative_humidity, ylim= c( 98, 101), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
  abline(a=0, b=0, col= 'red')
- abline(a=95, b=0, col='green')
- abline(a=95.25, b=0, col= 'red')
+ abline(a=100, b=0, col='green')
+ #abline(a=95.25, b=0, col= 'red')
+ 
+ 
+ plot(relative_humidity, ylim= c( -100, 1), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
+ abline(a=0, b=0, col= 'red')
+ abline(a=100, b=0, col='green')
+ #abline(a=95.25, b=0, col= 'red')
+ 
+ detach(properties)
+ detach(uiuc_weather_raw2)
+ 
+ #############################################################################################################################################
+ #DATA SET 3
+attach(uiuc_weather_raw3)
+attach(properties)
+ 
+ #Air temperature Plot
+ plot(air_temperature, ylim=c(100,350),main = "Air temperature through time", xlab = "Time", ylab= "Air Temperature (K)" , type= 'l', col= 'blue')
+ abline(a=193.15, b= 0, col='red')
+ abline(a=333.15 , b=0, col= 'green')
+ 
+ 
+ plot(relative_humidity, ylim= c(0, 105), type = 'l', col='blue')
+ abline(a=0, b=0, col= 'red')
+ abline(a=100, b=0, col='green')
+ 
+ #Relative humidity Plot
+ plot(relative_humidity, ylim= c(0, 105), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
+ abline(a=0, b=0, col= 'red')
+ abline(a=100, b=0, col='green')
+ #abline(a=95.25, b=0, col= 'purple')
+ 
+ #zoom of relative humidity
+ plot(relative_humidity, ylim= c(99, 101), type = 'l', col='blue', main= "Percentage of relative humidity through time", xlab= "Time", ylab="Relative Humidity (%)")
+ abline(a=0, b=0, col= 'red')
+ abline(a=100, b=0, col='green')
+ # abline(a=95.25, b=0, col= 'red')
+ detach(properties)
+ detach(uiuc_weather_raw3)
  
  
  
