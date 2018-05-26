ec443b635a23b63dae52d590b90a94d5de267bd5
Zhixing Guo <zhixing5@illinois.edu>
2018-05-26 16:14:56 -0500

    Code and image with corrected location for AZ.

--- daymet_comparison.R
+++ daymet_comparison.R
@@ -10,6 +10,28 @@ library(fitdistrplus)
 library(daymetr)
 
 
+# # getting data from the link and the corresponding data from daymet.
+# all_weather_data <- function(link){
+#   # Getting data from the link
+#   select <- dplyr::select
+#   weather_raw <- fromJSON(txt = link)
+#   weather <- weather_raw$properties
+  
+# Downloading DAYMET data for: the weather station in the link at latitude/longitude   
+df <- download_daymet(site = "UA-MAC AZMET Weather Station",
+                      lat = as.numeric(33.0745666667),
+                      lon = as.numeric(-111.9750833333),
+                      start = 2016,
+                      end = 2017,
+                      internal = TRUE)
+  
+df_data1 <- df[6]
+df_data <- data.frame(df_data1)
+  
+daymet <- df_data %>% mutate(date = ymd(paste0(data.year, '-01-01')) 
+                            + days(df_data$data.yday -1))
+  
+
 met <- readr::read_csv("data/uiuc_met.csv")
 unique(met$site)
 
@@ -19,7 +41,7 @@ AZ_met <- met %>% filter(site == "UA-MAC AZMET Weather Station") %>%
   select(meantime, date, temp)
 
 
-n = AZ_met %>% left_join(new)
+n = AZ_met %>% left_join(daymet)
 m <- n %>% mutate(temp = temp) %>% 
   dplyr::select(meantime, temp, tmax = data.tmax..deg.c., 
                 tmin = data.tmin..deg.c.) %>% 
